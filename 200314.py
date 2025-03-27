65c3e3885c2b5ca7ec85fa88cdda5fc0b36b0e97
vsaddr-github <46874948+vsaddr-github@users.noreply.github.com>
2025-03-26 22:15:03 -0400

    added references and output marked with tyope of color space

--- 200314.py
+++ 200314.py
@@ -3,17 +3,24 @@
 # https://medium.com/@vladstesttarget
 # email: vlads.test.target@gmail.com
 
+# version 1.1 3/26/2025 Vlads Test Target Scale changed to 100
+# extra reference https://www.easyrgb.com/en/math.php
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import os
 
-# Conversion mode flags
 show_rgb_text = True  # Toggle for RGB text overlay
+# Conversion mode flags
 use_prophoto = False
 use_srgb_d50 = True
 apply_gamma = True
-black_mask=False  # mask not compliant colors 
+black_mask=True # mask colors not compliant
+threshold=0.035
+
+num_clip=0
+conversion=""
 
 # Load the file
 file_path = "F210418.txt"
@@ -39,8 +46,9 @@ df[['XYZ_X', 'XYZ_Y', 'XYZ_Z']] = df[['XYZ_X', 'XYZ_Y', 'XYZ_Z']].astype(float)
 
 # Find the highest value in XYZ and normalize by it
 max_xyz_value = df[['XYZ_X', 'XYZ_Y', 'XYZ_Z']].values.max()
-print(f"Maximum XYZ value: {max_xyz_value}")
-max_xyz_value = 76.01
+print(f"Maximum XYZ value from data: {max_xyz_value}")
+max_xyz_value = 100# 80.63 #76.01
+print(f"Maximum XYZ value actually used: {max_xyz_value}")
 xyz_normalized = df[['XYZ_X', 'XYZ_Y', 'XYZ_Z']] / max_xyz_value
 
 
@@ -51,29 +59,41 @@ def gamma_correct(r, g, b):
     return correct(r), correct(g), correct(b)
 
 # Convert XYZ to RGB (sRGB D65, sRGB D50, or ProPhotoRGB)
+# see also http://www.brucelindbloom.com/index.html?Eqn_ChromAdapt.html
+# https://computergraphics.stackexchange.com/questions/13878/what-are-the-exact-math-steps-to-convert-srgb-d65-to-lab-d50-colorspace
 def xyz_to_rgb(sample_id,x, y, z, prophoto=False, srgb_d50=False, gamma=False):
+    global num_clip,conversion
     if prophoto:
         r =  1.3459433*x - 0.2556075*y - 0.0511118*z
         g = -0.5445989*x + 1.5081673*y + 0.0205351*z
         b =  0.0000000*x + 0.0000000*y + 1.2118128*z
     elif srgb_d50:
+        # https://www.image-engineering.de/library/technotes/958-how-to-convert-between-srgb-and-ciexyz
         r =  3.1338561*x - 1.6168667*y - 0.4906146*z
         g = -0.9787684*x + 1.9161415*y + 0.0334540*z
         b =  0.0719453*x - 0.2289914*y + 1.4052427*z
     else:
+        # https://www.easyrgb.com/en/math.php   XYZ → Standard-RGB  D65 
         r =  3.2406*x - 1.5372*y - 0.4986*z
         g = -0.9689*x + 1.8758*y + 0.0415*z
         b =  0.0557*x - 0.2040*y + 1.0570*z
-      
+        
+     
     if not (0 <= r <= 1 and 0 <= g <= 1 and 0 <= b <= 1):
-        #sample_id = df.iloc[i]['SAMPLE_ID']
-        #original_xyz = df.iloc[i][['XYZ_X', 'XYZ_Y', 'XYZ_Z']].tolist()
         # we apply gamma correctin to unclippped value exclusively to be able to 
         # match those clipped patches with ones shown on screen
         r1, g1, b1 = gamma_correct(r, g, b)
-        print(f"Patch {sample_id:<3}, unclipped  RGB=({r1*255: >+7.3f}, {g1*255: >+7.3f}, {b1*255: >+7.3f}) XYZ {x} {y} {z} ")
+        num_clip += 1
+        print(f"{num_clip: 4}. Patch {sample_id:<3}, unclipped  RGB=({r1*255: >+7.3f}, {g1*255: >+7.3f}, {b1*255: >+7.3f}) XYZ {x} {y} {z} ")
         if black_mask:
-           r, g, b = 0, 0, 0
+            if not ( (0-threshold) <= r <= (1+threshold) and (0-threshold) <= g <= (1+threshold) and (0-threshold) <= b <= (1+threshold)):
+               brightness = 0.299 * r + 0.587 * g + 0.114 * b
+               if brightness < 0.5:
+                   r, g, b = 0, 0, 0
+               else:
+                   r, g, b = 1, 1, 1
+            else:
+               r, g, b = max(0, min(1, r)), max(0, min(1, g)), max(0, min(1, b))   
         else:
            r, g, b = max(0, min(1, r)), max(0, min(1, g)), max(0, min(1, b))
     else:  
@@ -83,6 +103,16 @@ def xyz_to_rgb(sample_id,x, y, z, prophoto=False, srgb_d50=False, gamma=False):
     return r, g, b
 
 colors = []
+
+
+if use_prophoto:
+    conversion="ProPhotoRGB" # efficincy 
+elif use_srgb_d50:
+    conversion="sRGB_d50"
+else:
+    conversion="sRGB_d65"
+
+
 for i, (x, y, z) in enumerate(xyz_normalized.values):
     sample_id = df.iloc[i]['SAMPLE_ID']
     r, g, b = xyz_to_rgb(sample_id, x, y, z, prophoto=use_prophoto, srgb_d50=use_srgb_d50, gamma=apply_gamma)
@@ -120,11 +150,11 @@ for col_num in range(columns_per_row):
 import string
 for row_num in range(rows):
     ax.text(-0.5, row_num + 0.5, string.ascii_uppercase[row_num], ha='right', va='center', fontsize=8, fontweight='bold', color='0.5')
-plt.title(f"IT8 Chart - Visualized from XYZ Data (286 patches, 22 per row) WF charge #: {it8name} - {script_name}. By VLADS TEST TARGET", fontsize=16, pad=30)
+plt.title(f"IT8 Chart - Visualized from XYZ Data (286 patches, 22 per row) WF#: {it8name} Scale {max_xyz_value}. {script_name}. {conversion} nc-{num_clip}. By VLADS TEST TARGET", fontsize=16, pad=30)
 plt.subplots_adjust(top=0.5)
 plt.tight_layout()
 
 from datetime import datetime
 timestamp = datetime.now().strftime("%Y%m%dT%H-%M-%S")
-plt.savefig(f"{it8name}_{script_name}_chart_{timestamp}_m{max_xyz_value}_black_mask{black_mask}_rgbval{show_rgb_text}.png", dpi=300)
+plt.savefig(f"{it8name}_{script_name}_chart_{timestamp}_m{max_xyz_value}_black_mask{black_mask}_{conversion}_{num_clip}.png", dpi=300)
 plt.show()
--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 # it8_viewer
 IT8ProfileView
 
-This will read data file provided by Wolf Faust along with IT8 target and show on screen RGB equivalent of XYZ data stored in file.
+This will read data file provided by Wolf Faust along with his IT8 transparency target and show on the screen RGB equivalent of XYZ data stored in file. The png file is also written on disk for review late
+
+# Sample png files included
+
+In the following files XYZ data has been scaled by factor 100 
+
+F210418_200314.py_chart_20250326T21-37-14_m100_black_maskTrue_ProPhotoRGB_0.png - converted to ProPhotoRGB
+
+F210418_200314.py_chart_20250326T21-37-37_m100_black_maskTrue_sRGB_d65_58.png - converted to sRGB with D65
+ 
+F210418_200314.py_chart_20250326T21-58-22_m100_black_maskTrue_sRGB_d50_57.png - converted to sRGB with D50
+
+
+# Python version: 
+
+$python -V
+Python 3.10.11
+
+# Running
+
+It is expected that data file "F210418.txt" is located in the same folder. 
+File name is hardcoded for simplicity 
+
+ python 200314.py
